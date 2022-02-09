e5158c12fc7df4b3ea9d2dd975989d8f78aa1705
Elattari Mohssine <43855033+MohssineElAttari@users.noreply.github.com>
2022-02-10 00:05:16 +0100

    update in method test1

--- src/test/java/MyTestClass.java
+++ src/test/java/MyTestClass.java
@@ -1,10 +1,13 @@
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.junit.jupiter.api.Assertions.fail;
 
 public class MyTestClass {
     @Test
     public void test1(){
+//        assertTrue((10 * 2) ==20); // true
+        assertTrue((10 * 2) ==30); // false
 
     }
 
