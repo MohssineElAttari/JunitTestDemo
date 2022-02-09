e71c34106e84c9ddf138a97a68338a0cffbb157a
Elattari Mohssine <43855033+MohssineElAttari@users.noreply.github.com>
2022-02-09 23:50:21 +0100

    add new method test1

--- src/test/java/MyTestClass.java
+++ src/test/java/MyTestClass.java
@@ -1,2 +1,10 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.fail;
+
 public class MyTestClass {
+    @Test
+    public void test1(){
+        fail("couco");
+    }
 }
