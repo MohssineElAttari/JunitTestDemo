2ee80e2bd7da77fac16bfd457ae215280ea85f79
Elattari Mohssine <43855033+MohssineElAttari@users.noreply.github.com>
2022-02-10 09:18:08 +0100

    new update in method class test

--- src/test/java/MyTestClass.java
+++ src/test/java/MyTestClass.java
@@ -1,14 +1,25 @@
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
-import static org.junit.jupiter.api.Assertions.assertTrue;
-import static org.junit.jupiter.api.Assertions.fail;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class MyTestClass {
     @Test
     public void test1(){
-//        assertTrue((10 * 2) ==20); // true
-        assertTrue((10 * 2) ==30); // false
+//        assertTrue((10 * 2) ==30); // false
+        assertTrue((10 * 2) ==20); // true
 
     }
-
+    @ParameterizedTest
+    @ValueSource(ints = 8)
+    public void test2(int nbr){
+        //test length
+        assertEquals(nbr,"mohssine".length());
+    }
+    @ParameterizedTest
+    @ValueSource(ints = {10,10})
+    public void test3(int nbr){
+        assertTrue((nbr * 2) ==20); // true
+    }
 }
