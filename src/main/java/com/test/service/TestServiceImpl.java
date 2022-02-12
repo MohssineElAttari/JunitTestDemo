d2c26c67d0d09c33c0dddda184bc0a53b2b261c3
Elattari Mohssine <43855033+MohssineElAttari@users.noreply.github.com>
2022-02-12 23:34:11 +0100

    update test service impl

--- src/main/java/com/test/service/TestServiceImpl.java
+++ src/main/java/com/test/service/TestServiceImpl.java
@@ -1,2 +1,8 @@
-package com.test.service;public class TestServiceImpl {
+package com.test.service;
+
+public class TestServiceImpl implements ITestService{
+    @Override
+    public int multiple(int a, int b) {
+        return a*b;
+    }
 }
