b9369a571e50cb992f486db8d1a7432e64d01320
Elattari Mohssine <43855033+MohssineElAttari@users.noreply.github.com>
2022-02-12 23:34:56 +0100

    new update in service impl test

--- src/test/java/com/test/service/TestServiceImplTest.java
+++ src/test/java/com/test/service/TestServiceImplTest.java
@@ -1,4 +1,15 @@
+package com.test.service;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.exceptions.base.MockitoException;
+
 import static org.junit.jupiter.api.Assertions.*;
 class TestServiceImplTest {
-  
+    TestServiceImpl testService = new TestServiceImpl();
+    @Test
+    public void multiple() {
+       assertTrue( testService.multiple(5,9)==45);
+    }
+
 }
